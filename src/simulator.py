92f3c37c2958a0e8ba307ac4a3be619d6faca31c
Lazlo <lazlopaul17@gmail.com>
2025-12-18 13:15:12 -0800

    simulator saving data and running day by day

--- src/simulator.py
+++ src/simulator.py
@@ -831,8 +831,8 @@ def run_simulation(
     # Store forecast prices used
     da_schedule.da_price_forecast = np.array(da_prices.values)
     da_schedule.rt_price_forecast = np.array(rt_prices.values)
-
-    da_schedules[first_sim_date] = da_schedule
+    # Getting just first day from day ahead
+    da_schedules[first_sim_date] = da_schedule[:24*4]
 
     print(f"\n{'=' * 60}")
     print("RUNNING REAL-TIME SIMULATION")
